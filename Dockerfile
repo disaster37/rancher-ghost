0b2bb0da521d3e208a31092c706cbe1ab04c2021
Sébastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2015-11-18 21:26:47 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM webcenter/rancher-stack-base:latest
+MAINTAINER Sebastien LANGOUREAUX <linuxworkgroup@hotmail.com>
+
+ENV NODEJS_VERSION 0.10
+ENV NPM_VERSION 1.4.21
+ENV GHOST_VERSION 0.7.1
+
+ENV NODE_ENV=production
+ENV USE_GLUSTER true
+
+
+# Install node
+RUN curl -sL https://deb.nodesource.com/setup_${NODEJS_VERSION} | bash -
+RUN apt-get install -y nodejs
+RUN npm install -g npm@${NPM_VERSION}
+
+# Install ghost
+RUN mkdir -p /opt/ghost && \
+  cd /tmp && \
+  curl -L -o ghost.zip https://ghost.org/zip/ghost-${GHOST_VERSION}.zip && \
+  unzip ghost.zip -d /opt/ghost && \
+  rm -f ghost.zip && \
+  npm install --production --prefix /opt/ghost
+
+RUN npm install pg mysql
+
+# Add account
+RUN groupadd ghost
+RUN useradd -s /bin/false -g ghost -d /opt/ghost ghost
+RUN chown -R ghost:ghost /opt/ghost
+
+# Add init script
+ADD assets/setup/supervisor-ghost.conf
+RUN mkdir /app
+ADD assets/init.py /app/init.py
+ADD assets/run /app/run
+RUN chmod +x /app/run
+
+
+# CLEAN APT
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+WORKDIR /opt/ghost
+PORT 2368
+VOLUME ["/opt/ghost/content/data"]
+CMD ["/app/run"]
---
+++ assets/init.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+import json
+import os
+import re
+import shutil
+import sys
+import time
+from rancher_metadata import MetadataAPI
+
+__author__ = 'Sebastien LANGOUREAUX'
+
+GHOST_PATH = '/opt/ghost'
+
+class ServiceRun():
+
+  def set_config(self, db_type, db, db_port, db_host, db_user, db_password, url, port, mail_transport, mail_host, mail_ssl, mail_port, mail_user, mail_password, mail_from_address, mail_service, mail_key, mail_key_id):
+
+    global GHOST_PATH
+
+    # Check database parameters
+    if db_type is None or db_type == '':
+        raise KeyError("You must set the database type")
+    if db is None or db == '':
+        raise KeyError("You must set the database name")
+    if (db_type == 'mysql') or (db_type == 'postgresql'):
+        if db_user is None or db_user == '':
+            raise KeyError("You must set the user to access on database")
+        if db_password is None or db_password == '':
+            raise KeyError("You must set the password to access on database")
+        if db_port is None or db_port == '':
+            raise KeyError("You must set the port to access on database server")
+        if db_host is None or db_host == '':
+            raise KeyError('You must set the host to access on database server')
+
+    # Check node parameters
+    if url is None or url == '':
+        raise KeyError("You must set the URL of your Ghost blog")
+    if port is None or port == '':
+        raise KeyError("You must set the port to access of your Ghost blog")
+
+
+    # Create Json
+
+    json_config = {}
+    json_config['production'] = {}
+
+
+    # Set the database parameter
+    if db_type == "sqlite":
+        json_config['production']['database'] = {}
+        json_config['production']['database']['client'] = 'sqlite3'
+        json_config['production']['database']['connection'] = {}
+        json_config['production']['database']['connection']['filename'] = 'path.join(__dirname, \'/content/data/' + db + '.db\')'
+        json_config['production']['database']['debug'] = 'false'
+    elif db_type == "postgresql":
+        json_config['production']['database'] = {}
+        json_config['production']['database']['client'] = 'pg'
+        json_config['production']['database']['connection'] = {}
+        json_config['production']['database']['connection']['host'] = db_host
+        json_config['production']['database']['connection']['port'] = db_port
+        json_config['production']['database']['connection']['user'] = db_user
+        json_config['production']['database']['connection']['password'] = db_password
+        json_config['production']['database']['connection']['database'] = db
+        json_config['production']['database']['connection']['charset'] = 'utf8'
+        json_config['production']['database']['debug'] = 'false'
+    elif db_type == "mysql":
+        json_config['production']['database'] = {}
+        json_config['production']['database']['client'] = 'mysql'
+        json_config['production']['database']['connection'] = {}
+        json_config['production']['database']['connection']['host'] = db_host
+        json_config['production']['database']['connection']['port'] = db_port
+        json_config['production']['database']['connection']['user'] = db_user
+        json_config['production']['database']['connection']['password'] = db_password
+        json_config['production']['database']['connection']['database'] = db
+        json_config['production']['database']['connection']['charset'] = 'utf8'
+        json_config['production']['database']['debug'] = 'false'
+    else:
+        raise KeyError("The database type must be sqlite, mysql or postgresql")
+
+    # Set node parameters
+    json_config['production']['server'] = {}
+    json_config['production']['server']['host'] = '0.0.0.0'
+    json_config['production']['server']['port'] = port
+    json_config['production']['url'] = url
+
+
+    # Set mail setting
+    json_config['production']['mail'] = {}
+    if mail_host is not None and mail_host != '':
+        json_config['production']['mail']['options'] = {}
+        json_config['production']['mail']['options']['host'] = mail_host
+        if mail_transport is None or mail_transport == '':
+            raise KeyError("You must set the mail protocol")
+        if mail_ssl is None or mail_ssl == '':
+            raise KeyError("You must set if you should use encrypted connection with your mail server")
+        if mail_port is None or mail_port == '':
+            raise KeyError("You must set the mail server port")
+        json_config['production']['mail']['transport'] = mail_transport
+        json_config['production']['mail']['options']['port'] = mail_port
+        json_config['production']['mail']['options']['secureConnection'] = mail_ssl
+
+        if mail_user is not None and mail_user != '' and mail_password is not None and mail_password != '' :
+            json_config['production']['mail']['options']['auth'] = {}
+            json_config['production']['mail']['options']['auth']['user'] = mail_user
+            json_config['production']['mail']['options']['auth']['pass'] = mail_password
+    if mail_service == 'mailgun' and mail_user is not None and mail_user != '' and mail_password is not None and mail_password != '' :
+        json_config['production']['mail']['transport'] = 'SMTP'
+        json_config['production']['mail']['options'] = {}
+        json_config['production']['mail']['options']['service'] = 'Mailgun'
+        json_config['production']['mail']['options']['auth'] = {}
+        json_config['production']['mail']['options']['auth']['user'] = mail_user
+        json_config['production']['mail']['options']['auth']['pass'] = mail_password
+    if mail_service == 'ses' and mail_key is not None and mail_key != '' and mail_key_id is not None and mail_key_id != '' :
+        json_config['production']['mail']['transport'] = 'SES'
+        json_config['production']['mail']['options'] = {}
+        json_config['production']['mail']['options']['AWSAccessKeyID'] = mail_key_id
+        json_config['production']['mail']['options']['AWSSecretKey'] = mail_key
+    if mail_service == 'gmail' and mail_user is not None and mail_user != '' and mail_password is not None and mail_password != '' :
+        json_config['production']['mail']['transport'] = 'SMTP'
+        json_config['production']['mail']['options'] = {}
+        json_config['production']['mail']['options']['auth'] = {}
+        json_config['production']['mail']['options']['auth']['user'] = mail_user
+        json_config['production']['mail']['options']['auth']['pass'] = mail_password
+    if mail_from_address is not None and mail_from_address != '':
+        json_config['production']['mail']['fromaddress'] = mail_from_address
+
+
+    json_config = json.dumps(json_config)
+
+    config = '''
+        // # Ghost Configuration
+        // Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+        // Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
+
+        var path = require('path'),
+        config;
+
+        config = ''' + str(json_config) + ''';
+        module.exports = config;
+    '''
+
+    self.add_end_file(GHOST_PATH + '/config.js', config)
+
+
+  def replace_all(self, file, searchRegex, replaceExp):
+    """ Replace String in file with regex
+    :param file: The file name where you should to modify the string
+    :param searchRegex: The pattern witch must match to replace the string
+    :param replaceExp: The string replacement
+    :return:
+    """
+
+    regex = re.compile(searchRegex, re.IGNORECASE)
+
+    f = open(file,'r')
+    out = f.readlines()
+    f.close()
+
+    f = open(file,'w')
+
+    for line in out:
+      if regex.search(line) is not None:
+        line = regex.sub(replaceExp, line)
+
+      f.write(line)
+
+    f.close()
+
+
+  def add_end_file(self, file, line):
+    """ Add line at the end of file
+    :param file: The file where you should to add line to the end
+    :param line: The line to add in file
+    :return:
+    """
+    with open(file, "a") as myFile:
+        myFile.write("\n" + line + "\n")
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    if os.path.isfile(GHOST_PATH + '/config.js'):
+        shutil.copy2(GHOST_PATH + '/config.js', GHOST_PATH + '/config.js.org')
+        os.remove(GHOST_PATH + '/config.js')
+
+    service = ServiceRun()
+
+    if os.getenv('DB_TYPE') is None:
+        db_type = 'sqlite'
+    else :
+        db_type = os.getenv('DB_TYPE')
+
+    db_host = os.getenv('DB_HOST')
+    if os.getenv('DB_DATABASE') is None:
+        if os.getenv('DB_ENV_DB') is None:
+            db = 'ghost'
+        else :
+            db = os.getenv('DB_ENV_DB')
+            db_host = 'db'
+    else:
+        db = os.getenv('DB_DATABASE')
+
+    if os.getenv('DB_ENV_USER') is not None:
+        db_user = os.getenv('DB_ENV_USER')
+    else:
+        db_user = os.getenv('DB_USER')
+
+    if os.getenv('DB_ENV_PASS') is not None:
+        db_pass = os.getenv('DB_ENV_PASS')
+    else:
+        db_pass = os.getenv('DB_PASS')
+
+    if os.getenv('DB_PORT_5432_TCP_PORT') is not None:
+        db_port = os.getenv('DB_PORT_5432_TCP_PORT')
+    else:
+        db_port = os.getenv('DB_PORT')
+
+    if os.getenv('GHOST_PORT') is None:
+        port = '2368'
+    else:
+        port = os.getenv('GHOST_PORT')
+
+    if os.getenv('MAIL_NAME') is not None:
+        mail_host = 'mail'
+    else:
+        mail_host = os.getenv('MAIL_NAME')
+
+
+
+    service.set_config(db_type, db, db_port, db_host, db_user, db_pass, os.getenv('GHOST_URL'), port, os.getenv('MAIL_TRANSPORT'), mail_host, os.getenv('MAIL_SSL'), os.getenv('MAIL_PORT'), os.getenv('MAIL_USER'), os.getenv('MAIL_PASSWORD'), os.getenv('MAIL_FROM_ADDRESS'), os.getenv('MAIL_SERVICE'), os.getenv('MAIL_SES_KEY'), os.getenv('MAIL_SES_KEY_ID'))
